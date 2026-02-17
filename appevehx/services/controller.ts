8ce74043b220b3a75d9fc4586138481a93b0db06
Acrisson <acrisson2@gmail.com>
2026-02-17 19:56:26 -0300

    Teste de api on na web

--- appevehx/services/controller.ts
+++ appevehx/services/controller.ts
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000";
+const API_URL = "https://server-evehx2.onrender.com";
 
 export async function getRecords() {
   const response = await fetch(`${API_URL}/usuarios`);
